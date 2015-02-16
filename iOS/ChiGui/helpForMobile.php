6cdf19aad85f7585ad193b4ecb845e0f917d1d98
倪 李俊 <e.joshua.s.e@gmail.com>
2015-02-16 11:12:22 +0800

    ChiGui init

---
+++ iOS/ChiGui/helpForMobile.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<html>
+<head>
+<!--以下meta表面使用iOS Safari的适配-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+
+<title>ChiGui Help</title>
+</head>
+<body>
+</h1>About ChiGui</h1>
+<div style='width: 90%;margin: auto;'>
+ChiGui is an application running on iOS devices, providing a platform of drawing geometry graphs with straight lines and circles.
+</div>
+</body>
+</html>
