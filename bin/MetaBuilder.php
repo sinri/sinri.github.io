f463eaf1ddcfcd977164eac15344a08d6d5d1c06
Sinri Edogawa <e.joshua.s.e@gmail.com>
2018-10-06 16:49:05 +0800

    Refine

--- bin/MetaBuilder.php
+++ bin/MetaBuilder.php
@@ -2,4 +2,6 @@
 require_once __DIR__.'/MetaBuilderFunc.php';
 
 refreshMeta(__DIR__.'/../meta.json',__DIR__.'/../data');
-refreshMeta(__DIR__.'/../WatchmanNee/meta.json',__DIR__.'/../WatchmanNee/data');
+
+// As is done
+// refreshMeta(__DIR__.'/../WatchmanNee/meta.json',__DIR__.'/../WatchmanNee/data');
